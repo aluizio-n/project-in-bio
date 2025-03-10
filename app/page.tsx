09ee97cced6703095679e5852f529eae3a48eaa9
Aluizio Neto <aluizio.sln@gmail.com>
2025-03-10 08:11:30 -0300

    feat: add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Hero from "./components/landing-page/hero";
+import Header from "./components/landing-page/header";
 
 export default function Home() {
   return (
     <div className="max-w-7xl m-auto">
+      <Header />
       <Hero />
-      {/*  <Header />
-       <Hero />
-       <VideoExplantion />
-       <Pricing />
-       <FQA /> */}
+      {/*
+      <VideoExplantion />
+      <Pricing />
+      <FQA /> 
+       */}
     </div>
   );  
 }
